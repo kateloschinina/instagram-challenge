f2505dd33360e5f810c6f4ec6136e670b64a6976
Ekaterina Loshchinina <KateLoschinina@ekaterinasmbp5.home>
2017-02-26 15:16:56 +0000

    User can delete pictures. Tests pass.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,9 +12,16 @@ class PicturesController < ApplicationController
       Picture.create!(picture_params)
       redirect_to '/pictures'
     else
-      flash[:notice] = 'You haven not uploaded anything yet'
+      flash[:notice] = 'You have not uploaded anything yet'
       redirect_to '/pictures/new'
     end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    flash[:notice] = 'Your picture was successfully deleted'
+    redirect_to '/pictures'
   end
 
   private
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -25,9 +25,20 @@ feature 'pictures' do
       click_link 'Add a picture'
       attach_file('picture[image]', 'lib/images/default.jpg')
       click_button 'Upload picture'
-      save_and_open_page
       expect(page).to have_css("img[src*='default.jpg']")
       expect(page).not_to have_content 'Your instagram is empty.'
+    end
+  end
+
+  context 'deleting pictures' do
+    scenario 'user can delete his own picture' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      attach_file('picture[image]', 'lib/images/default.jpg')
+      click_button 'Upload picture'
+      click_link 'Delete'
+      expect(page).not_to have_css("img[src*='default.jpg']")
+      expect(page).to have_content 'Your instagram is empty.'
     end
   end
 end

e9163ec674e0c063a42d3ee889169f7c7124331f
Ekaterina Loshchinina <KateLoschinina@ekaterinasmbp5.home>
2017-02-26 23:11:13 +0000

    Introduction of CSS.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,6 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/lib/images/default.jpg"
+  has_attached_file :image, :styles => { :medium => "600x600>", :thumb => "100x100>" }, :default_url => "/lib/images/default.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end

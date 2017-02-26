040b39b081d3e6cd204337575487f4b2ca852af5
Ekaterina Loshchinina <KateLoschinina@ekaterinasmbp5.home>
2017-02-26 14:58:08 +0000

    User can add pictures, which are displayed on index page. Tests pass.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,23 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    if params[:picture]
+      Picture.create!(picture_params)
+      redirect_to '/pictures'
+    else
+      flash[:notice] = 'You haven not uploaded anything yet'
+      redirect_to '/pictures/new'
+    end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:image)
+  end
 end
--- spec/features/pictures_feature_spec.rb
+++ spec/features/pictures_feature_spec.rb
@@ -18,4 +18,16 @@ feature 'pictures' do
       expect(page).not_to have_content 'Your instagram is empty.'
     end
   end
+
+  context 'adding a new picture' do
+    scenario 'shows user a form to add new picture, then displays it' do
+      visit '/pictures'
+      click_link 'Add a picture'
+      attach_file('picture[image]', 'lib/images/default.jpg')
+      click_button 'Upload picture'
+      save_and_open_page
+      expect(page).to have_css("img[src*='default.jpg']")
+      expect(page).not_to have_content 'Your instagram is empty.'
+    end
+  end
 end

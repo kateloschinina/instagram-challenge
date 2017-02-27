ee98d5e1d3b0a9e554d76f28dc0396737e2bca62
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-5.local>
2017-02-27 15:43:49 +0000

    Further changes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,8 @@ class CommentsController < ApplicationController
     redirect_to pictures_path
   end
 
+  # Check which one is working!
+
   def create
     @picture = Picture.find(params[:picture_id])
     @picture.comments.create(review_params)
